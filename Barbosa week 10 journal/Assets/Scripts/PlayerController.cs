834e014937790f94e12a80dfc339a81514fc4a14
dg015 <bruno.barbosa8@yahoo.com>
2024-11-13 14:01:24 -0500

    Completed task 2

--- Barbosa week 10 journal/Assets/Scripts/PlayerController.cs
+++ Barbosa week 10 journal/Assets/Scripts/PlayerController.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private Rigidbody2D rigdbody;
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private BoxCollider2D colider;
     [SerializeField] private float speed;
+    [SerializeField] private FacingDirection currentDirection;
     public enum FacingDirection
     {
         left, right
@@ -14,22 +15,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        colider = GetComponent<BoxCollider2D>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        Debug.Log(Input.GetAxis("Horizontal"));
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"),0);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb.AddForce(playerInput * speed);
     }
 
     public bool IsWalking()
@@ -43,6 +47,22 @@ public class PlayerController : MonoBehaviour
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (Input.GetAxis("Horizontal") > 0.1)
+        {
+
+            currentDirection = FacingDirection.right;
+            return currentDirection;
+            
+        }
+        if (Input.GetAxis("Horizontal") < -0.1)
+        {
+
+            currentDirection = FacingDirection.left;
+            return currentDirection;
+
+
+        }
+        else
+            return currentDirection;
     }
 }
