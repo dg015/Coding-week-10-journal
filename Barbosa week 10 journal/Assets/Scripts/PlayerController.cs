c62784ad3e48c32ee7389cca239d392f40fa1244
dg015 <bruno.barbosa8@yahoo.com>
2024-11-23 18:16:23 -0500

    task 2 complete

--- Barbosa week 10 journal/Assets/Scripts/PlayerController.cs
+++ Barbosa week 10 journal/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [Header("Physics components")]
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private BoxCollider2D colider;
+    [Header("Walking")]
     [SerializeField] private float speed;
     [SerializeField] private FacingDirection currentDirection;
+    [Header("Raycast")]
     [SerializeField] private LayerMask layer;
     [SerializeField] private float jumpHeight = 5;
-    [SerializeField] private List<ContactPoint2D> contacts = new List<ContactPoint2D>();
     [SerializeField] private int raycastDistance = 2;
     [Space]
     [Space]
+    [Header("Jump varaibles")]
     //jump
     [SerializeField] private float ApexTimeJump = 2;
     [SerializeField] private float ApexHeightJump = 2 ;
-
-
+    [SerializeField] private float terminalSpeed;
     public enum FacingDirection
     {
         left, right
@@ -30,8 +30,23 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        colider = GetComponent<BoxCollider2D>();    
+
     }
+
+
+    private void TerminalVelocityCheckr()
+    {
+        // create new variable and attach it to speed
+        Vector2 playerVelocity = rb.velocity;
+        //clamp new variable to terminal speed, its negative because its falling
+        playerVelocity.y = Mathf.Clamp(rb.velocity.y, -1 *terminalSpeed, 1000);
+
+        //use variable as the velocity of the player
+        rb.velocity = playerVelocity;
+
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -40,36 +55,44 @@ public class PlayerController : MonoBehaviour
         // The input from the player needs to be determined and
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
-        //Debug.Log(Input.GetAxis("Horizontal"));
         Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"),0);
-        MovementUpdate(playerInput);       
+        MovementUpdate(playerInput);
+        
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
 
         float gravity = -2 * ApexHeightJump / (ApexTimeJump * ApexTimeJump);
+        float jumpVelocity = 2 * ApexHeightJump / ApexTimeJump;
 
 
         rb.AddForce(playerInput * speed);
         float velocityX = rb.velocity.x;
 
 
-
         if (IsGrounded() && Input.GetKey(KeyCode.Space) )
         {
-            rb.gravityScale = 1;
+            rb.gravityScale = 0;
             //rb.AddForce(new Vector2(0, jumpHeight),ForceMode2D.Impulse); old jump
             //new jump bellow
 
-            float jumpVelocity = 2 * ApexHeightJump / ApexTimeJump;
-            rb.velocity = new Vector2(velocityX, gravity);
+            rb.velocity = new Vector2(velocityX, jumpVelocity);
 
+            
+        }
+        else if (!IsGrounded())
+        {
+            rb.velocity = new Vector2(velocityX, rb.velocity.y + gravity * Time.deltaTime);
+            rb.gravityScale = 0;
+            TerminalVelocityCheckr();
+            
         }
         else
         {
-            rb.gravityScale = 0;
+            rb.gravityScale = 1;
         }
+
     }
 
     public bool IsWalking()
@@ -79,22 +102,23 @@ public class PlayerController : MonoBehaviour
             return true;
         }
         else
+        {
             return false;
-        
+        }
+
     }
     public bool IsGrounded()
     {
         
         RaycastHit hit;
         Debug.DrawLine(transform.position, transform.position + Vector3.down * raycastDistance, Color.white);
-        Physics.Raycast(transform.position, transform.position + Vector3.down, out hit, raycastDistance, layer);
+        //Physics.Raycast(transform.position, transform.position + Vector3.down, out hit, raycastDistance, layer);
         if (Physics2D.Raycast(transform.position, transform.position + Vector3.down, raycastDistance, layer))
         {
             return true;
         }
         else
         {
-
             return false;
         }
     }
